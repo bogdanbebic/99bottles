50b8d779c06359abdb32d0e2e8efd2d2b5cfec5f
Bogdan Bebic <bogdanbebic@microsoft.com>
2021-09-23 09:51:21 +0200

    Add VS solution, gitignore, tests

---
+++ 99bottles/99bottles/Bottles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _99bottles
+{
+    public class Bottles
+    {
+        public string Verse(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Verses(int start, int end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Song()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 99bottles/99bottles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _99bottles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
